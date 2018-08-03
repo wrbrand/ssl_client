8ec42834e0449bf82516e0e0c130b89e0ba5d43b
Will Brand <will.brand@gmail.com>
2018-08-03 02:01:43 -0400

    Capturing server response

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"./ssl"
-	"encoding/binary"
-	"fmt"
+		"fmt"
 	"math/rand"
 	"net"
 	"time"
+	"encoding/binary"
 )
 
 func main() {
@@ -16,10 +16,11 @@ func main() {
 }
 
 func tryHandshake(random ssl.ClientRandom, session_id ssl.SessionID) {
-	conn, err := net.Dial("tcp", "example.com:443")
+	timeout := 30 * time.Second
 
+	conn, err := net.Dial("tcp", "example.com:443")
 	if err != nil {
-		fmt.Print(err)
+		panic(err)
 	}
 
 	helloBody := ssl.NewClientHello(ssl.ProtocolVersion{3, 3}, random, session_id)
@@ -27,4 +28,20 @@ func tryHandshake(random ssl.ClientRandom, session_id ssl.SessionID) {
 	helloMessage := ssl.NewSSLPlainText(ssl.HANDSHAKE, ssl.ProtocolVersion { Major: 3, Minor: 3}, helloHandshake.Serialization)
 
 	binary.Write(conn, binary.BigEndian, helloMessage.Serialization.Serialize())
+
+	defer conn.Close()
+
+	response := make([]byte, 0, 65536)
+	tmp := make([]byte, 65536)
+	conn.SetReadDeadline((time.Now().Add(timeout)))
+
+	n, err := conn.Read(tmp)
+	if err != nil {
+		panic(err)
+	}
+	response = append(response, tmp[:n]...)
+
+	conn.Close();
+
+	fmt.Printf("%x", response)
 }
